d868afc6c4cf6907a91b36703c96b0a6afab5f53
Nguyen Hai Bay <bayn2k6@gmail.com>
2025-03-14 12:58:28 +0700

    first fail :<<

--- Tank/Tank code.cpp
+++ Tank/Tank code.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
-
+#include <SDL.h>
 using namespace std;
+
+void render(){
+    SDL_SetRenderDrawColor(renderer,128,128,128,255);
+
+    SDL_RenderClear(renderer);
+
+    SDL_SetRenderDrawColor(renderer,0,0,0,255);
+
+    fot)
+}
 
 int main()
 {
